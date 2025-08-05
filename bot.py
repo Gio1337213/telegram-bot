a9430a3dc982dff2e1c3db0e65ba06ce4184ae64
Gio <danya2004400@gmail.com>
2025-08-05 21:12:18 +0700

    Обновил ссылки на каналы

--- bot.py
+++ bot.py
@@ -9,9 +9,12 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(types.InlineKeyboardButton(text="💡 Канал про ИТ", url="https://t.me/your_it_channel"))
-    keyboard.add(types.InlineKeyboardButton(text="📈 Канал про инвестиции", url="https://t.me/your_invest_channel"))
-    await message.answer("Выберите интересующую тему:", reply_markup=keyboard)
+    keyboard.add(types.InlineKeyboardButton(text="Спорт", url="https://t.me/sportsoda"))
+    keyboard.add(types.InlineKeyboardButton(text="Новости Профкома", url="https://t.me/profkomsoda"))
+    keyboard.add(types.InlineKeyboardButton(text="ОТиПБ", url="https://t.me/your_invest_channel"))
+    keyboard.add(types.InlineKeyboardButton(text="Фабрика идей", url="https://t.me/your_invest_channel"))
+    keyboard.add(types.InlineKeyboardButton(text="Что такое БСА", url="https://t.me/your_invest_channel"))
+    await message.answer("Выберите интересующую вас тему:", reply_markup=keyboard)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
